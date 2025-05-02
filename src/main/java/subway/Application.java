69e61e99028bf6285aed251dc86bfca3da8f8f1e
Taekeundo <pleasepose@gmail.com>
2025-05-02 20:37:58 +0200

    refactor(App): add initializer() to main
    - To initialize stations and lines when app starts
    - Before running, need data.

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,15 +1,16 @@
 package subway;
 
 import subway.view.InputView;
+import subway.config.DataInitializer;
 import subway.controller.AppController;
 import subway.controller.RouteController;
-
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
 
+        DataInitializer.init();
         InputView inputView = new InputView(scanner);
         RouteController routeController = new RouteController();
         AppController appController = new AppController(inputView, routeController);
