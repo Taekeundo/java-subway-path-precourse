e953b3ecd16faecdefdbbe0305781f4fb6ce4cd2
Taekeundo <pleasepose@gmail.com>
2025-05-02 18:19:04 +0200

    Refactor(controller): RouteController to validate stations

--- src/main/java/subway/controller/RouteController.java
+++ src/main/java/subway/controller/RouteController.java
@@ -14,8 +14,7 @@ public class RouteController {
         Station departure = StationRepository.findbyName(departName);
         Station destination = StationRepository.findbyName(destinationName);
 
-        if (departure == null || destination == null) {
-            OutputView.printError("존재하지 않는 역입니다.");
+        if (!validateStations(departure, destination)) {
             return;
         }
 
@@ -27,11 +26,25 @@ public class RouteController {
         Station departure = StationRepository.findbyName(departName);
         Station destination = StationRepository.findbyName(destinationName);
 
-        if (departure == null || destination == null) {
-            OutputView.printError("존재하지 않는 역입니다.");
+        if (!validateStations(departure, destination)) {
             return;
         }
+
         ResultRoute result = searchRoute.calcMinimumTime(departure, destination);
         OutputView.printResult(result);
+    }
+
+    private boolean validateStations(Station departure, Station destination) {
+        if (departure == null || destination == null) {
+            OutputView.printError("존재하지 않는 역입니다.");
+            return false;
+        }
+
+        if (departure.equals(destination)) {
+            OutputView.printError("출발역과 도착역이 동일합니다.");
+            return false;
+        }
+
+        return true;
     }
 }
