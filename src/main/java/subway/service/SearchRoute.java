68b9047b5dbd24d3b971662cca794d3b4dfa71f0
Taekeundo <pleasepose@gmail.com>
2025-05-02 20:51:29 +0200

    refactor(service): calc route using section
    - Instead of fixed values(previous), use Line's sections to build graph with actual distance and time from initializer

--- src/main/java/subway/service/SearchRoute.java
+++ src/main/java/subway/service/SearchRoute.java
@@ -2,6 +2,7 @@ package subway.service;
 
 import subway.domain.Line;
 import subway.domain.LineRepository;
+import subway.domain.Section;
 import subway.domain.Station;
 
 import org.jgrapht.Graph;
@@ -16,25 +17,25 @@ public class SearchRoute {
 
     public ResultRoute calcShortestDistance(Station departureStation, Station destinationStation) {
         Graph<Station, DefaultWeightedEdge> graph = createGraphByDistance();
-        return calculateRoute(graph, departureStation, destinationStation);
+        return calculateRoute(graph, departureStation, destinationStation, true);
     }
 
     public ResultRoute calcMinimumTime(Station departureStation, Station destinationStation) {
         Graph<Station, DefaultWeightedEdge> graph = createGraphByTime();
-        return calculateRoute(graph, departureStation, destinationStation);
+        return calculateRoute(graph, departureStation, destinationStation, false);
     }
 
-    private ResultRoute calculateRoute(Graph<Station, DefaultWeightedEdge> graph, Station departure, Station destination) {
+    private ResultRoute calculateRoute(Graph<Station, DefaultWeightedEdge> graph, Station departure, Station destination, boolean isDistance) {
         DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstra = new DijkstraShortestPath<>(graph);
 
         if (dijkstra.getPath(departure, destination) == null) {
-            return new ResultRoute(new ArrayList<>(), 0, 0); // 경로가 없을 경우
+            return new ResultRoute(new ArrayList<>(), 0, 0); // 경로 없음 처리
         }
 
         List<Station> path = dijkstra.getPath(departure, destination).getVertexList();
         int totalValue = (int) dijkstra.getPath(departure, destination).getWeight();
 
-        if (graph.equals(createGraphByDistance())) {
+        if (isDistance) {
             return new ResultRoute(path, totalValue, 0);
         }
         return new ResultRoute(path, 0, totalValue);
@@ -44,17 +45,16 @@ public class SearchRoute {
         Graph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
 
         for (Line line : LineRepository.lines()) {
-            List<Station> stations = line.getStations();
-
-            for (int i = 0; i < stations.size() - 1; i++) {
-                Station from = stations.get(i);
-                Station to = stations.get(i + 1);
+            for (Section section : line.getSections()) {
+                Station from = section.getFrom();
+                Station to = section.getTo();
+                int distance = section.getDistance();
 
                 graph.addVertex(from);
                 graph.addVertex(to);
 
                 DefaultWeightedEdge edge = graph.addEdge(from, to);
-                graph.setEdgeWeight(edge, 10);
+                graph.setEdgeWeight(edge, distance);
             }
         }
         return graph;
@@ -64,17 +64,16 @@ public class SearchRoute {
         Graph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
 
         for (Line line : LineRepository.lines()) {
-            List<Station> stations = line.getStations();
-
-            for (int i = 0; i < stations.size() - 1; i++) {
-                Station from = stations.get(i);
-                Station to = stations.get(i + 1);
+            for (Section section : line.getSections()) {
+                Station from = section.getFrom();
+                Station to = section.getTo();
+                int time = section.getTime();
 
                 graph.addVertex(from);
                 graph.addVertex(to);
 
                 DefaultWeightedEdge edge = graph.addEdge(from, to);
-                graph.setEdgeWeight(edge, 5);
+                graph.setEdgeWeight(edge, time);
             }
         }
         return graph;
